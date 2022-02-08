ab5a856ed53250e5e590f4b86e25d649c0ebcd19
4war <fedro_777@mail.ru>
2022-02-08 11:15:16 +0400

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SectionTournamentComponent } from './section-tournament/section-tournament.component';
 import { SectionPlayerComponent } from './section-player/section-player.component';
+import {Routes, RouterModule} from "@angular/router";
+import {appRoutes} from "../routes";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SectionTournamentComponent,
-    SectionPlayerComponent
+    SectionPlayerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+﻿import {Routes} from "@angular/router";
+import {SectionTournamentComponent} from "./app/section-tournament/section-tournament.component";
+import {SectionPlayerComponent} from "./app/section-player/section-player.component";
+
+export const appRoutes: Routes = [
+  {path: 'tournament', component: SectionTournamentComponent},
+  {path: 'player', component: SectionPlayerComponent},
+
+  {path: '', redirectTo: '/tournament', pathMatch: 'full'},
+]
+
+
